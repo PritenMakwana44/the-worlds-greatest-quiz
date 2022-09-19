d934e427ed4af70e7dafb784681382056eeb9b10
PritenMakwana44 <priten.makwana7@gmail.com>
2022-09-19 19:51:32 +0000

    half complete login/register function

--- run.py
+++ run.py
@@ -13,4 +13,48 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('the_worlds_greatest_quiz')
 
 bank = SHEET.worksheet('bank')
+login = SHEET.worksheet('login')
+
+print("Welcome to the worlds greatest quiz")
+print("Instructions\n First login or register\n To play the game type true or false on each question\n The highest score is 100 points\n Each time you play there will be new questions!\n No cheating ;)")
+
+def login_register():
+    inital_question = input("Do you have an account? y/n: ")
+    if inital_question == "n":
+        while True:
+            username = input("Enter Username: ")
+            password = input("Enter Password: ")
+            confirm_password = input("confirm Password: ")
+            if password == confirm_password:
+                login.append_row(username)
+                login.append_row(password)
+                inital_question ="y"
+                break
+            
+            print("Passwords do not match!")
+    
+    if inital_question == "y":
+        while True:
+            login_username = input("username: ")
+            login_password = input("password: ")
+            print("welcome back!")
+            break
+            
+
+
+
+
+
+    
+
+
+def main():
+    login_register()
+
+
+main()
+
+        
+        
+
 
