9cb29942956a6a7c15cdbbaddc24ebe8ceed5045
PritenMakwana44 <priten.makwana7@gmail.com>
2022-10-06 19:24:56 +0000

    Added clean titles with sleep function for print at beginning with instructions and fixed capital letters

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import random
+from time import sleep
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -24,15 +26,39 @@ answer_col = bank.col_values(2)
 
 
 
-print("Welcome to the worlds greatest quiz")
-print("Instructions\n First login or register\n To play the game type true or false on each question\n The highest score is 100 points\n Each time you play there will be new questions!\n No cheating ;)")
+print("Welcome To The Worlds Greatest Quiz")
+sleep(1)
+print("Instructions")
+sleep(1)
+print("...")
+sleep(1)
+print("First Login or Regiester")
+sleep(1)
+print("...")
+sleep(1)
+print("To play the game type True or False on each question")
+sleep(1)
+print("...")
+sleep(1)
+print("Each time you play there will be new questions!")
+sleep(1)
+print("...")
+sleep(1)
+print("...")
+sleep(1)
+print("No cheating ;)")
+sleep(1)
+print("...")
+print(" ")
+
+
 
 def create_new_user():
     username_list = login.col_values(1)
     password_list = login.col_values(2)
     points_list = login.col_values(3)
     
-    username = input("Enter new Username:")
+    username = input("Enter New Username:")
     
     while True:
         try:
@@ -48,41 +74,43 @@ def create_new_user():
             create_new_user()
         if username in username_list:
             print(f"'{username}' already exists")
-            username = input("Enter new Username:")
+            username = input("Enter New Username:")
         else:
             print("Username Accepted...")
             break
 
 
-    password = input("Enter new Password: ")
+    password = input("Enter New Password: ")
     while True:
         for s in password:        
                 if (s.isspace()) == True:
                     print(f"Please fill in a valid password")
-                    password = input("Enter new Password: ")
+                    password = input("Enter New Password: ")
                     
             
         if (len(password.strip()) == 0):
             print(f"Please fill in a valid password")
-            password = input("Enter new Password: ")
+            password = input("Enter New Password: ")
                     
             
                     
         else:
             points = 0
-            print(f"Creating Account for {username}...")
+            print(f"Creating Account For {username}...")
             update_worksheet = []                
             update_worksheet.append(username)
             update_worksheet.append(password)
             update_worksheet.append(points)
             login.append_row(update_worksheet)
-            print(f"Account created Successfully!")
+            print(f"Account Created Successfully!")
             logon()
             break
             
 def logon():
-    print(f"Please login")
-    username = input('username: ')
+    print(f"Please Login")
+    print("...")
+
+    username = input('Username: ')
     points = 0
     username_list = login.col_values(1)
     password_list = login.col_values(2)
@@ -93,7 +121,7 @@ def logon():
             if password_list[i] == password:
                 print('Success')
                 points = points_list[i]
-                print(f"You last scored: {points} points")
+                print(f"You Last Scored: {points} Points")
                 question_answer(username)
                 break
                     
@@ -102,7 +130,7 @@ def logon():
                 login_register()
                     
         elif i == len(username_list)-1:
-                print("Username not found")
+                print("Username Not Found!")
                 login_register()
                 break
     
@@ -110,7 +138,7 @@ def logon():
                 
 
 def login_register():
-    inital_question = input("Do you have an account? y/n: ")
+    inital_question = input("Do You Have An Account? y/n: ")
     if inital_question == "n":
         create_new_user()
         
@@ -152,43 +180,44 @@ def question_answer(username):
     new_question_list_length = len(new_question_list)
     count = 0
     while count < new_question_list_length:
-
-        
         question_cell = bank.find(new_question_list[count])
         answer_cell = bank.cell(question_cell.row, question_cell.col + 1).value
+        print(f" ")
         print(new_question_list[count])
         count +=1
         answer_input = input('Enter True or False: ').upper()
+        print(f" ")
+        print(f"Result:")
         while True:
             if answer_input.upper() == 'TRUE': 
                 if answer_cell.upper() == answer_input.upper():
-                    print("correct")
+                    print("Correct! - 10 points :)")
                     increment = int(login.cell(qa_username_cell.row, qa_username_cell.col + 2).value)
                     increment = increment + 10
                     login.update_cell(qa_username_cell.row, qa_username_cell.col + 2, increment)
                     break
                     
                 elif answer_cell.upper() != answer_input.upper():
-                    print("Incorrect")
+                    print("Incorrect - 0 points :(")
                     break
                 
             elif answer_input.upper() == 'FALSE': 
                 if answer_cell.upper() == answer_input.upper():
-                    print("correct")
+                    print("Correct! - 10 points :)")
                     increment = int(login.cell(qa_username_cell.row, qa_username_cell.col + 2).value)
                     increment = increment + 10
                     login.update_cell(qa_username_cell.row, qa_username_cell.col + 2, increment)
                     break
                     
                 elif answer_cell.upper() != answer_input.upper():
-                    print("Incorrect")
+                    print("Incorrect - 0 points :(")
                     break
 
             else:
                 print("Invalid Input")
                 answer_input = input('Enter True or False: ').upper()
 
-
+        print(f" ")
     get_points = int(login.cell(qa_username_cell.row, qa_username_cell.col + 2).value)
     print(f"You now have {get_points} points")
     
